c69ad6a671993cc09fd67e091aadb07f762398ae
GabrielMcryu <gabrielmcryu@gmail.com>
2026-03-20 12:31:58 +0300

    feature: developed user endpoints

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
+    path('create/', create_user, name='create-user'),
+    path('list/', list_users, name='list-users'),
+    path('list/<int:pk>/', get_user, name='get-user'),
+    path('update/<int:pk>/', update_user, name='update-user'),
+    path('delete/<int:pk>/', delete_user, name='delete-user'),
 ]
